e3a695b0cb02d80529a610e3feee9989289cc557
Wagner <wagner@bintb.com>
2021-10-12 10:51:52 -0300

    List Categories Use Case and Controller

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,16 @@
-import { request, response, Router } from "express";
+import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const catetoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const categories = catetoriesRepository.list();
-  return response.json(categories);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
