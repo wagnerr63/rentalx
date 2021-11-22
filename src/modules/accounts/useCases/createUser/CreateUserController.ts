7a28e5cb5c4547fb192810ef1e261bb7a2bb5060
wagnerbintb <wagner@bintb.com>
2021-11-22 14:56:44 -0300

    create user route, controller and use case

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, username, email, password, driver_license } = request.body;
+
+    const createUserUseCase = container.resolve(CreateUserUseCase);
+    try {
+      await createUserUseCase.execute({
+        name,
+        username,
+        email,
+        password,
+        driver_license,
+      });
+
+      return response.status(201).send();
+    } catch (error) {
+      return response
+        .status(400)
+        .json({ message: error.message || "Unexpected error." });
+    }
+  }
+}
+
+export { CreateUserController };
